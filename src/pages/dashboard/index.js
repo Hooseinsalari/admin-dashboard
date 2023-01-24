1ec659ba63e06714a70c82a62a51c114664b905d
Hossein Salari <hossein.salari002@gmail.com>
2023-01-24 16:42:45 +0330

    create Header component

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// component
+import Header from '../../components/Header';
+
+// mui
+import { Box } from '@mui/material';
+
 const Dashboard = () => {
     return (
-        <div>
-            dashboard
-        </div>
+        <Box m="18px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
     );
 };
 
