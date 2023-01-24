85831f08689c8dfb6760a912cc08c8b5a6313192
Hossein Salari <hossein.salari002@gmail.com>
2023-01-24 18:49:28 +0330

    create team page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useMode } from "./theme";
 import Topbar from "./components/common/Topbar";
 import Sidebar from "./components/common/Sidebar";
 import Dashboard from "./pages/dashboard";
+import Team from "./pages/team"
 
 // mui
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -27,6 +28,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
